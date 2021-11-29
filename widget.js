ea35317ac743a7f146ad5e8456d6badcfbfcad8f
CodeByJeeens <codebyjeeens@gmail.com>
2021-11-29 18:18:07 +0100

    Widget update

--- widget.js
+++ widget.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: magic;
-const dataUrl = "https://makma.github.io/scriptable-stocks/result.json";
+const dataUrl = "https://codewithjeeens.github.io/TaksBar/result.json";
 
 let widget = await createWidget();
 Script.setWidget(widget);
@@ -13,19 +13,22 @@ async function createWidget() {
 
     const data = await new Request(dataUrl).loadJSON();
 
-    let titleRow = widget.addText(`Losers ${data.timestamp}`);
+    let titleRow = widget.addText(--TaksBar--);
     titleRow.font = Font.boldSystemFont(15);
     titleRow.textColor = Color.white();
-    widget.addSpacer(5);
 
-    for (i = 0; i < 7; i++) {
-        const looser = data.loosers[i];
-        let row = leftColumn.addText(`
-            ${looser.ticker}
-            ${looser.change}
-        `);
-        row.font = Font.semiboldSystemFont(14);
-    }
+    let Spacer0 = listwidget.adspacer()
+
+    let Name = listwidget.addText(`${data.Name}`)
+
+    let Spacer1 = listwidget.adspacer()
+
+    let Name = listwidget.addText(`${data.Guthaben}`)
+
+    let Spacer0 = listwidget.adspacer()
+
+    let Name = listwidget.addText(`${data.Bonuspunkte}`)
+
 
     let gradient = new LinearGradient()
 
